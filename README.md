8f199bf71dec211baf2e662afcd2594561b9b7fa
dingshu <ds303077135@gmail.com>
2014-09-27 21:54:29 +0800

    version 0.1

--- README.md
+++ README.md
@@ -2,48 +2,13 @@ charch.js 中文排版工具
 ======
 [github.com/quietshu/charch](github.com/quietshu/charch)
 
-### 特点
-- 仅作格式处理，CSS 中文样式请使用 Typo、Han CSS 库
-- 替换公共引号“”、‘’为中文直角引号「」、『』
-- 中西文衔接处添加一个半角空格
-- 对齐方式设置为左右对齐，`word-break`
-- 修复 `p` 标签下默认忽略换行符的问题
-
 ### 致谢
 [https://github.com/vinta/paranoid-auto-spacing](https://github.com/vinta/paranoid-auto-spacing)
-
 
 ### 协议
 
 - 作者：[Shu Ding](github.com/quietshu)
-- 版本：1.3
+- 版本：0.1
 - 协议：[The MIT License](http://opensource.org/licenses/MIT)
 
 ### 用法
-
-1. include `charch.js`
-  
-2. 在需要排版的地方（`<p>` 或是上层元素）加入 class `charch_post`。例如：
-
-	原代码：
-
-		<div>
-			<p>半夜写代码</p>
-			<p>真蛋疼</p>
-		</div>
-
-	应改成：
-
-		<div class="charch_post">
-			<p>半夜写代码</p>
-			<p>真蛋疼</p>
-		</div>
-  
- 
-### Todos
-
-- 如何才能更灵活处理字距问题？比如「ACM-ICPC」和「1 - 2」
-- 字符集、符号类别需要完善，需要支持所有 CJK 字符和特殊符号
-- 中英文混输 word-wrap 如何解决
-- 两个标点悬挂 bug 待修复
-- 或许可以进一步制作成一个阅读网页优化插件
--- src/charch.js
+++ src/charch.js
@@ -1,56 +1,89 @@
-(function () {
-    window.onload = function () {
-        var posts = document.getElementsByClassName("charch_post");
-        var posts_num = posts.length;
-        for (var i = 0; i < posts_num; ++i) {
-            charch_typeset_post(posts[i]);
+// pangu.js https://github.com/vinta/paranoid-auto-spacing
+(function(i){var j=/^(code|pre|textarea)$/i;var d=/^(a|del|pre|s|strike|u)$/i;var f=/^(br|hr|i|img|pangu)$/i;var c=/^(div|h1|h2|h3|h4|h5|h6|p)$/i;function e(k){var l=k.parentNode;while(l.nodeName.search(/^(html|head|body|#document)$/i)===-1){if((l.getAttribute("contenteditable")==="true")||(l.getAttribute("g_editable")==="true")){return true}else{if(l.nodeName.search(j)>=0){return true}else{l=l.parentNode}}}return false}function b(o,m){var n=o.childNodes;for(var k=0;k<n.length;k++){var l=n[k];if(l.nodeType!==8&&l.textContent){return l===m}}}function a(o,m){var n=o.childNodes;for(var k=n.length-1;k>-1;k--){var l=n[k];if(l.nodeType!==8&&l.textContent){return l===m}}}function h(m){var l=m;var k;m=m.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(["'])/ig,"$1 $2");m=m.replace(/(["'])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig,"$1 $2");m=m.replace(/(["']+)(\s*)(.+?)(\s*)(["']+)/ig,"$1$3$5");m=m.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(#(\S+))/ig,"$1 $2");m=m.replace(/((\S+)#)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig,"$1 $3");l=m;k=l.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<\[\{\(]+(.*?)[>\]\}\)]+)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig,"$1 $2 $4");m=k;if(l===k){m=m.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<>\[\]\{\}\(\)])/ig,"$1 $2");m=m.replace(/([<>\[\]\{\}\(\)])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig,"$1 $2")}m=m.replace(/([<\[\{\(]+)(\s*)(.+?)(\s*)([>\]\}\)]+)/ig,"$1$3$5");m=m.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([a-z0-9`@&%=\$\^\*\-\+\|\/\\])/ig,"$1 $2");m=m.replace(/([a-z0-9`~!%&=;\|\,\.\:\?\$\^\*\-\+\/\\])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig,"$1 $2");return m}function g(l){var t=false;var k=document.evaluate(l,document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);var n=k.snapshotLength;var v;for(var s=n-1;s>-1;--s){var r=k.snapshotItem(s);if(e(r)){v=r;continue}var u=h(r.data);if(r.data!==u){t=true;r.data=u}if(v){if(r.nextSibling){if(r.nextSibling.nodeName.search(f)>=0){v=r;continue}}var w=r.data.toString().substr(-1)+v.data.toString().substr(0,1);var o=h(w);if(w!==o){t=true;var m=v;while(m.parentNode&&m.nodeName.search(d)===-1&&b(m.parentNode,m)){m=m.parentNode}var p=r;while(p.parentNode&&p.nodeName.search(d)===-1&&a(p.parentNode,p)){p=p.parentNode}if(p.nextSibling){if(p.nextSibling.nodeName.search(f)>=0){v=r;continue}}if(p.nodeName.search(c)===-1){if(m.nodeName.search(d)===-1){if((m.nodeName.search(j)===-1)&&(m.nodeName.search(c)===-1)){if(v.previousSibling){if(v.previousSibling.nodeName.search(f)===-1){v.data=" "+v.data}}else{v.data=" "+v.data}}}else{if(p.nodeName.search(d)===-1){r.data=r.data+" "}else{var q=document.createElement("pangu");q.innerHTML=" ";if(m.previousSibling){if(m.previousSibling.nodeName.search(f)===-1){m.parentNode.insertBefore(q,m)}}else{m.parentNode.insertBefore(q,m)}if(!q.previousElementSibling){if(q.parentNode){q.parentNode.removeChild(q)}}}}}}}v=r}return t}i.text_spacing=function(k){return h(k)};i.page_spacing=function(){var l="/html/head/title/text()";g(l);var m="/html/body//*/text()[normalize-space(.)]";["script","style","textarea"].forEach(function(n){m+='[translate(name(..),"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz")!="'+n+'"]'});var k=g(m);return k};i.element_spacing=function(n){var p;if(n.indexOf("#")===0){var m=n.slice(1);p='id("'+m+'")//text()'}else{if(n.indexOf(".")===0){var l=n.slice(1);p='//*[contains(concat(" ",normalize-space(@class)," "), " '+l+' ")]//text()'}else{var o=n;p="//"+o+"//text()"}}var k=g(p);return k}}(window.pangu=window.pangu||{}));
+(function(){
+    'use strict';
+
+    var insert_style_sheet = function () {
+        var style = document.createElement("style");
+        style.innerHTML = ".charch {" +
+            "   word-break: normal;" +
+            "   word-warp: break-word;" +
+            "   line-height: 1.8em;" +
+            "   text-align: justify" +
+            "}" +
+            ".no_line_height {" +
+            "   line-height: inherit" +
+            "}" +
+            "p.charch_indent, .charch_indent p {" +
+            "   text-indent: 2em" +
+            "}" +
+            "a.charch_link, .charch_link a {" +
+            "   text-decoration: none;" +
+            "   border-bottom: 1px solid" +
+            "}" +
+            "p.charch_pseudo, .charch_pseudo p {" +
+            "   text-indent: 0" +
+            "}" +
+            "p.charch_pseudo::first-letter, .charch_pseudo p::first-letter {" +
+            "   font-size: 2.8em;" +
+            "   line-height: 1.2em;" +
+            "   float: left" +
+            "}";
+        (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(style);
+    };
+
+    var for_each_specific_element = function (p, tag, func) {
+        if (p.nodeName === tag)
+            return func(p);
+        var children_num = p.childNodes.length;
+        for(var i = 0; i < children_num; ++i) {
+            var add_ons = for_each_specific_element(p.childNodes[i], tag, func);
+            i += add_ons;
+            children_num += add_ons;
         }
-    }
+        return 0;
+    };
 
-    var check_tag_name = function (el) {
-        if (el.tagName == "DIV" ||
-            el.tagName == "P" ||
-            el.tagName == "SPAN" ||
-            el.tagName == "B" ||
-            el.tagName == "A" ||
-            el.tagName == "UL" ||
-            el.tagName == "LI" ||
-            el.tagName == "BLOCKQUOTE" ||
-            el.tagName == "I" ||
-            el.tagName == "STRONG" ||
-            el.tagName == "ITALIC" ||
-            el.tagName == "SUP" ||
-            el.tagName == "SUB")
-            return true;
-        return false;
-    }
+    var replace_bracket = function (p) {
+        for_each_specific_element(p, "#text", function (t) {
+            var s = t.data;
+            s = s.replace(/“/g, "「");
+            s = s.replace(/”/g, "」");
+            s = s.replace(/‘/g, "『");
+            s = s.replace(/’/g, "』");
+            t.data = s;
+            return 0;
+        });
+    };
 
-    var charch_typeset_post = function(post) {
-        if (post.childNodes) {
-            var par = post.childNodes.length;
-            for(var i = 0; i < par; ++i) {
-                if (check_tag_name(post.childNodes[i]))
-                    charch_typeset_post(post.childNodes[i]);
-                else if (!post.childNodes[i].tagName) {
-                    post.childNodes[i].nodeValue = replace_content(post.childNodes[i]);
-                }
-            }
-        } else if (check_tag_name(post)) {
-            post.innerHTML = replace_content(post.innerHTML);
-        }
-    }
+    var link_space = function (p) {
+        for_each_specific_element(p, "A", function (t) {
+            var space_before = document.createElement("span"), space_after = document.createElement("span");
+            space_before.innerText = space_after.innerText = " ";
+            t.parentNode.insertBefore(space_before, t);
+            t.parentNode.insertBefore(space_after, t.nextSibling);
+            return 2;
+        });
+    };
 
-    var replace_content = function(str){
-        var s   = str.nodeValue;
-        var len = s.length;
-        if(!len)
-            return s;
+    var charch_typeset_post = function (p) {
+        var className = " " + p.className + " ";
 
-        s = s.replace(/‘/g, "『");
-        s = s.replace(/’/g, "』");
-        s = s.replace(/“/g, "「");
-        s = s.replace(/”/g, "」");
+        if(className.indexOf(" no_cjk_latin_space ") === -1)
+            p.className += " charch_space";
 
-        return s;
-    }
+        if(className.indexOf(" no_bracket_replace ") === -1)
+            replace_bracket(p);
+
+        if(className.indexOf(" no_link_space ") === -1)
+            link_space(p);
+    };
+
+    var posts = document.getElementsByClassName("charch");
+    var posts_num = posts.length;
+    for (var i = 0; i < posts_num; ++i)
+        charch_typeset_post(posts[i]);
+
+    insert_style_sheet();
+    pangu.element_spacing(".charch_space");
 })();
