7f0b4cde0152b7932b10ea16e51eca5084b0528f
Alan Chan <github@zenan.ch>
2016-09-06 22:50:39 +0800

    Try an alt method

--- src/charch.js
+++ src/charch.js
@@ -12,12 +12,6 @@ module.exports = Charch;
 function Charch(document) {
     'use strict';
 
-    function isChinese(node) {
-        var re=/[^\u4e00-\u9fa5]/;
-        if(re.test(node.textContent)) return false;
-        return true;
-    }
-
     var insert_style_sheet = function () {
         var style       = document.createElement("style");
         style.innerHTML = ".charch {" +
@@ -82,6 +76,8 @@ function Charch(document) {
             s      = s.replace(/‘/g, "『");
             s      = s.replace(/’/g, "』");
             s      = s.replace(/([A-z]+)』/g, "$1'");
+            s      = s.replace(/([A-z]+)」.*?([A-z]+)/g, "$1”$2");
+            s      = s.replace(/([A-z]+)「.*?([A-z]+)/g, "$1“$2");
             t.data = s;
             return 0;
         });
@@ -100,7 +96,7 @@ function Charch(document) {
     var charch_typeset_post = function (p) {
         var className = " " + p.className + " ";
 
-        if (className.indexOf(" charch-bracket-replace ") !== -1 && (isChinese(p))) {
+        if (className.indexOf(" charch-bracket-replace ") !== -1) {
             replace_bracket(p);
         }
 
